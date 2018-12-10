5fd29693a4673dafa6ec692b9d004f1aacfdaea2
Chris <Chris@DESKTOP-L0UOBR8>
2018-12-10 14:15:57 -0800

    Started Laying out main window

--- AutoBackUpGUI/AutoBackUpGUI.cpp
+++ AutoBackUpGUI/AutoBackUpGUI.cpp
@@ -1,8 +1,55 @@
 #include "AutoBackUpGUI.h"
-#include <QtWidgets>
 
 AutoBackUpGUI::AutoBackUpGUI(QWidget *parent) : QMainWindow(parent){
-
-
 	ui.setupUi(this);
+	this->setFixedSize(480, 300);
+	this->setWindowTitle("AutoBackUp");
+
+	setupWatchedDirs();
+	setupWatchedDirsButtons();
+}
+
+void
+AutoBackUpGUI::setupWatchedDirs() {
+	watchedTitleLabel = new QLabel(this);
+	watchedTitleLabel->move(watchedX+5, watchedY-30);
+	watchedTitleLabel->resize(watchedW, 30);
+	watchedTitleLabel->setText("Watched Directories");
+	watchedTitleLabel->setFont(*fontTitle);
+
+	
+	watchedLabel = new QLabel(this);
+	watchedScroll = new QScrollArea(this);
+
+	watchedLabel->move(watchedX, watchedY);
+	watchedScroll->move(watchedX, watchedY);
+	
+	watchedLabel->resize(watchedW, watchedH);
+	watchedScroll->resize(watchedW, watchedH);
+	
+	watchedLabel->setText("test \n test \n test");
+	watchedLabel->setFont(*fontScroll);
+	
+	
+	watchedScroll->setWidget(watchedLabel);
+}
+
+void
+AutoBackUpGUI::setupWatchedDirsButtons() {
+	int h = 30;
+	
+	addButton = new QPushButton(this);
+	removeButton = new QPushButton(this);
+
+	addButton->move(watchedX, watchedY + 200);
+	removeButton->move(watchedX + watchedH/2, watchedY + 200);
+
+	addButton->resize(watchedW/2, h);
+	removeButton->resize(watchedW/2, h);
+
+	addButton->setText("Add");
+	addButton->setFont(*fontButton);
+	removeButton->setText("Remove");
+	removeButton->setFont(*fontButton);
+	
 }
--- AutoBackUpGUI/AutoBackUpGUI.h
+++ AutoBackUpGUI/AutoBackUpGUI.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <QtWidgets>
 #include <QtWidgets/QMainWindow>
 #include "ui_AutoBackUpGUI.h"
 
@@ -12,4 +12,23 @@ public:
 
 private:
 	Ui::AutoBackUpGUIClass ui;
+
+	QLabel *watchedTitleLabel; //widget that titles watchedLabel section
+	QLabel *watchedLabel; //widget to display directories currently being watched
+	QScrollArea *watchedScroll; //adds scroll functionality to widget displaying directories
+
+	QPushButton *addButton; //button widget to add new directories to watch
+	QPushButton *removeButton; //button widget to remove directories from being watched
+
+	QFont *fontScroll = new QFont("Arial", 12); //font for items in scroll area
+	QFont *fontButton = new QFont("Arial", 13, QFont::Bold); //font for buttons
+	QFont *fontTitle = new QFont("Arial", 14, QFont::Bold); //font for titles
+
+	const int watchedX = 20; //x-pos for watchedLabel
+	const int watchedY = 50; //y-pos for watchedLabel
+	const int watchedH = 200; //Height of watchedLabel
+	const int watchedW = 200; //width of watchedLabel
+
+	void setupWatchedDirs(); //sets up and configures watchedLabel & watchedScroll & watchedTitleLabel
+	void setupWatchedDirsButtons(); //sets up and configures addButton & removeButton
 };
